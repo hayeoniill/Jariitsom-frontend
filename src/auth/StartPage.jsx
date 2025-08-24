003fcc50bd9b087d91a811a8bbb178795eeb6a4a
sia <piao516@naver.com>
2025-08-24 15:12:33 +0900

    이미지경로

--- src/auth/StartPage.jsx
+++ src/auth/StartPage.jsx
@@ -3,17 +3,29 @@ import * as SP from "./StyledStartPage";
 import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <>
+      <SP.LogoImg
+        src={`${process.env.PUBLIC_URL}/images/Logo/logoSom.svg`}
+        alt="logo"
+      />
+      <SP.LogoTxt
+        src={`${process.env.PUBLIC_URL}/images/Logo/logoText.svg`}
+        alt="logo"
+      />
 
-    return (
-        <>
-            <SP.LogoImg src="/images/Logo/logoSom.svg" alt="logo" />
-            <SP.LogoTxt src="/images/Logo/logoText.svg" alt="logo" />
-            <SP.GoJoin onClick={() => navigate("/Join")}> 회원가입 </SP.GoJoin>
-            <SP.GoMain onClick={() => navigate("/Home")}> 비회원으로 시작하기 </SP.GoMain>
-            <SP.GoLogin onClick={() => navigate("/Login")}> 이미 회원이신가요? <SP.LogLink>로그인하기</SP.LogLink> </SP.GoLogin>
-        </>
-    );
-}
+      <SP.GoJoin onClick={() => navigate("/Join")}> 회원가입 </SP.GoJoin>
+      <SP.GoMain onClick={() => navigate("/Home")}>
+        {" "}
+        비회원으로 시작하기{" "}
+      </SP.GoMain>
+      <SP.GoLogin onClick={() => navigate("/Login")}>
+        {" "}
+        이미 회원이신가요? <SP.LogLink>로그인하기</SP.LogLink>{" "}
+      </SP.GoLogin>
+    </>
+  );
+};
 export default StartPage;
